6a4d1301618d553082976d3e181bd5be02f20a0a
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-01-24 17:13:40 +0100

    gui.py - changed to simplepyple

--- gui.py
+++ gui.py
@@ -1,4 +1,27 @@
 import simplepyble
+import binascii
+import asyncio
+
+
+async def content():
+    i = 0
+    for contents in service_characteristic_pair:
+        try:
+            print(f"{i}:")
+            r_content = peripheral.read(contents[0], contents[1])
+
+            print(f"Original contents: {r_content}")
+            print(f"Binary/Ascii contents: {binascii.b2a_hex(r_content, b' ', -2)}")
+            if contents[2]:
+                print(peripheral.descriptor_read(contents[0], contents[1], contents[2]))
+            print("\n")
+        except:
+            print("Error")
+            print('\n')
+        i += 1
+async def notify_me(pair):
+    service_uuid, characteristic_uuid = pair[3]
+    await peripheral.notify(service_uuid, characteristic_uuid, lambda data: print(f"Notification: {data}"))
 
 if __name__ == "__main__":
     adapters = simplepyble.Adapter.get_adapters()
@@ -11,8 +34,8 @@ if __name__ == "__main__":
     for i, adapter in enumerate(adapters):
         print(f"{i}: {adapter.identifier()} [{adapter.address()}]")
 
-    choice = int(input("Enter choice: "))
-    adapter = adapters[choice]
+    #choice = int(input("Enter choice: "))
+    adapter = adapters[0]
 
     print(f"Selected adapter: {adapter.identifier()} [{adapter.address()}]")
 
@@ -28,30 +51,40 @@ if __name__ == "__main__":
     print("Please select a peripheral:")
     for i, peripheral in enumerate(peripherals):
         print(f"{i}: {peripheral.identifier()} [{peripheral.address()}]")
+    for peripheral in peripherals:
+        if peripheral.identifier() == "Kanye Sweep":
+            print(f"Connecting to: {peripheral.identifier()} [{peripheral.address()}]")
+            peripheral.connect()
+            print(peripheral.identifier())
 
-    choice = int(input("Enter choice: "))
-    peripheral = peripherals[choice]
 
-    print(f"Connecting to: {peripheral.identifier()} [{peripheral.address()}]")
-    peripheral.connect()
+        else:
+            print(f"Peripheral Kanye Sweep not found.")
+
+    #choice = int(input("Enter choice: "))
+    #peripheral = peripherals[choice]
+
+    #print(f"Connecting to: {peripheral.identifier()} [{peripheral.address()}]")
+    #peripheral.connect()
 
     print("Successfully connected, listing services...")
     services = peripheral.services()
     service_characteristic_pair = []
     for service in services:
         for characteristic in service.characteristics():
-            service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
+            service_characteristic_pair.append((service.uuid(), characteristic.uuid(), characteristic.descriptors()))
 
     # Query the user to pick a service/characteristic pair
     print("Please select a service/characteristic pair:")
-    for i, (service_uuid, characteristic) in enumerate(service_characteristic_pair):
-        print(f"{i}: {service_uuid} {characteristic}")
+    for i, (service_uuid, characteristic, descriptor) in enumerate(service_characteristic_pair):
+        print(f"{i}: {service_uuid} {characteristic} {descriptor}")
 
-    choice = int(input("Enter choice: "))
-    service_uuid, characteristic_uuid = service_characteristic_pair[choice]
+    #choice = int(input("Enter choice: "))
+    #service_uuid, characteristic_uuid, characteristic_descriptors = service_characteristic_pair[choice]
 
     # Write the content to the characteristic
-    contents = peripheral.read(service_uuid, characteristic_uuid)
-    print(f"Contents: {contents}")
-
+    #contents = peripheral.read(service_uuid, characteristic_uuid)
+    #print(f"Original contents: {contents}")
+    asyncio.run(content())
+    #asyncio.run(notify_me(service_characteristic_pair))
     peripheral.disconnect()
